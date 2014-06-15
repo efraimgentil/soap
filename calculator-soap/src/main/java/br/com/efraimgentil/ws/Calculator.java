75cead2f496abe2d6a80387a377a42480d55c642
Efraim Gentil <efraim.gentil@gmail.com>
2014-06-15 17:20:08 -0300

    Add Calculator soap service

---
+++ calculator-soap/src/main/java/br/com/efraimgentil/ws/Calculator.java
@@ -0,0 +1,21 @@
+package br.com.efraimgentil.ws;
+
+public class Calculator {
+	
+	public double sum(double i , double j){
+		return i + j;
+	}
+	
+	public double minus(double i , double j){
+		return i - j;
+	}
+	
+	public double plus(double i , double j){
+		return i * j;
+	}
+	
+	public double divide(double i, double j){
+		return i / j;
+	}
+	
+}
